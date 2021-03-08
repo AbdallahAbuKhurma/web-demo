24162cb2c27a3df23ee8a2d44288823773c7d344
Abdallah Abu Khurma <abdallahabukhurma@gmail.com>
2021-03-08 22:06:37 +0200

    app.js added

---
+++ app.js
@@ -0,0 +1,18 @@
+function alertfunction(){
+    alert("wecome to our web page");
+
+}
+alertfunction();
+
+var meal = prompt("please enter a meal");
+function asking(){
+    if(meal == "breakfast")
+    {
+        alert("you entered breakfast")
+    }
+    else
+    {
+        alert("you did not enter breakfast")
+    }
+}
+asking();
